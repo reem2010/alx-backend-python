b56996519d965ab2d8a9ddc8c2c7cde00ff3647e
reem <tarekreem950@gmail.com>
2024-03-19 00:44:05 +0200

    basic async func

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""basic async module"""
+
+import asyncio
+import random
+
+
+async def wait_random(max_delay=10):
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
