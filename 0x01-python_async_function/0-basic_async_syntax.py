3c469ab9960ce19674d790c0285a9150be1530a3
reem <tarekreem950@gmail.com>
2024-03-19 00:48:05 +0200

    basic async func

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -6,6 +6,13 @@ import random
 
 
 async def wait_random(max_delay=10):
+    """wait_random
+    Args:
+        max_delay: the delay
+    Returns:
+        the delay
+    """
     delay = random.uniform(0, max_delay)
+
     await asyncio.sleep(delay)
     return delay
